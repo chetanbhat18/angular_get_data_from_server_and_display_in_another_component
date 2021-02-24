72cd3912ed4a696d83a898ab2728bc21d304cff3
Chetan Bhat <chetanbhat999@gmail.com>
2021-02-24 10:05:08 +0000

    Basic template

---
+++ src/app/a-navbar/a-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-navbar',
+  templateUrl: './a-navbar.component.html',
+  styleUrls: ['./a-navbar.component.scss']
+})
+export class ANavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneComponent } from './components/one/one.component';
+import { TwoComponent } from './components/two/two.component';
+import { ANavbarComponent } from './a-navbar/a-navbar.component';
+import { BFooterComponent } from './b-footer/b-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent,
+    ANavbarComponent,
+    BFooterComponent
   ],
   imports: [
     BrowserModule,

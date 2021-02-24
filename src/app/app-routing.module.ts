07dfcb9e0aaac5f93482e7939df70030aa733bd6
Chetan Bhat <chetanbhat999@gmail.com>
2021-02-24 11:51:48 +0000

    angular routing between two component
    routing and navigation through button clicks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './components/one/one.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TwoComponent } from './components/two/two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: OneComponent},
+  { path: 'two', component: TwoComponent},
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingcomponents = [ OneComponent,
+                                   PageNotFoundComponent,
+                                   TwoComponent
+                                 ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingcomponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OneComponent } from './components/one/one.component';
-import { TwoComponent } from './components/two/two.component';
 import { ANavbarComponent } from './a-navbar/a-navbar.component';
 import { BFooterComponent } from './b-footer/b-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OneComponent,
-    TwoComponent,
     ANavbarComponent,
-    BFooterComponent
+    BFooterComponent,
+    routingcomponents
   ],
   imports: [
     BrowserModule,
